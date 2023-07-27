e36a0d8f8e6259bc060503bb5d00348d91cd24c6
GACB-Ar <gerocb.lab@gmail.com>
2023-07-27 19:30:36 -0300

    resuelto la renderizacion de imagenes en home.html

--- blog_site/urls.py
+++ blog_site/urls.py
@@ -16,5 +16,6 @@ urlpatterns = [
     #articules
 ]
 
+# Serving media files during development
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- blog_site/views.py
+++ blog_site/views.py
@@ -3,10 +3,10 @@ from .models import Article
 
 # Create your views here.
 def home(request):
-    articles = Article.objects.all()
-    context = {'articles': articles}
+    articles = Article.objects.all()    
 
-    return render(request, 'home.html' , context)
+    return render(request, 'home.html' , {
+        'articles': articles})
 
 def about(request):
     return render(request, 'about.html')
